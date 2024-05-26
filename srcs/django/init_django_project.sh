fe1b82ba61492f34d37b84602fffa4deb1171850
lopjf <loris.penelon@outlook.com>
2024-05-26 17:55:20 +0200

    added wait for it script

--- srcs/django/init_django_project.sh
+++ srcs/django/init_django_project.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Source the .env file to make environment variables available
+source .env
+
+# Wait for the PostgreSQL database to be ready
+./wait-for-it.sh ${POSTGRES_HOST}:${POSTGRES_PORT} --timeout=30 --strict -- echo "Database is up"
+
 # Check if manage.py exists
 if [ ! -f "manage.py" ]; then
     # Initialize Django project
@@ -8,20 +14,12 @@ fi
 
 # update the settings.py file to add postgresql as the database
 sed -i "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE': 'django.db.backends.postgresql',/" myproject/settings.py
-sed -i "s/'NAME': BASE_DIR \/ 'db.sqlite3',/'NAME': 'your_database_name',/" myproject/settings.py
+sed -i "s/'NAME': BASE_DIR \/ 'db.sqlite3',/'NAME': '${POSTGRES_DB}',/" myproject/settings.py
 # Use a to append a line after a match
-sed -i "/'NAME': 'your_database_name',/a \        'USER': 'your_username'," myproject/settings.py
-sed -i "/'USER': 'your_username',/a \        'PASSWORD': 'your_password'," myproject/settings.py
-sed -i "/'PASSWORD': 'your_password',/a \        'HOST': 'db'," myproject/settings.py
-sed -i "/'HOST': 'db',/a \        'PORT': '5432'," myproject/settings.py
-
-
-# sed -i "s/'NAME': BASE_DIR \/ 'db.sqlite3',/'NAME': os.environ.get('POSTGRES_DB'),/" myproject/settings.py
-# # Use a to append a line after a match
-# sed -i "/'NAME': os.environ.get('POSTGRES_DB'),/a \        'USER': os.environ.get('POSTGRES_USER')," myproject/settings.py
-# sed -i "/'USER': os.environ.get('POSTGRES_USER'),/a \        'PASSWORD': os.environ.get('POSTGRES_PASSWORD')," myproject/settings.py
-# sed -i "/'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),/a \        'HOST': os.environ.get('POSTGRES_HOST')," myproject/settings.py
-# sed -i "/'HOST': os.environ.get('POSTGRES_HOST'),/a \        'PORT': os.environ.get('POSTGRES_PORT')," myproject/settings.py
+sed -i "/'NAME': '${POSTGRES_DB}',/a \        'USER': '${POSTGRES_USER}'," myproject/settings.py
+sed -i "/'USER': '${POSTGRES_USER}',/a \        'PASSWORD': '${POSTGRES_PASSWORD}'," myproject/settings.py
+sed -i "/'PASSWORD': '${POSTGRES_PASSWORD}',/a \        'HOST': '${POSTGRES_HOST}'," myproject/settings.py
+sed -i "/'HOST': '${POSTGRES_HOST}',/a \        'PORT': ${POSTGRES_PORT}," myproject/settings.py
 
 # Run Django development server
 python manage.py runserver 0.0.0.0:8000
