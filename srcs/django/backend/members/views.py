d84201ce103351e24e2287601b1458a026a2abfb
Courinha <antoniocourinha768@gmail.com>
2024-05-30 17:51:38 +0100

    Login working
    Login is working but still no connection from front end to back end, right now im using an html page in the backend to login

---
+++ srcs/django/backend/members/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+from django.http import HttpResponse
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse('Login successful')
+        else:
+            return HttpResponse('Login not successful')
+    else:
+        return render(request, 'authenticate/login.html')
+
