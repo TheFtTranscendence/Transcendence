9ed9056ec7923c9bc6478940344233482ba4f688
Courinha <antoniocourinha768@gmail.com>
2024-06-09 19:00:46 +0100

    API is working
    fixed problem with the CORS

--- srcs/django/backend/API/urls.py
+++ srcs/django/backend/API/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserList, UserDetail
+from .views import UserList, UserDetail, login_view
 
 urlpatterns = [
 	path('User', UserList.as_view()),
 	path('User/<int:pk>', UserDetail.as_view()),
+	path('loginUser', login_view)
 ]
--- srcs/django/backend/backend/settings.py
+++ srcs/django/backend/backend/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 
+	'corsheaders',
 	'rest_framework',
 	'API',
-	'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -49,44 +49,44 @@ REST_FRAMEWORK = {
 	),
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-# 	"http://0.0.0.0:3000",
-# ]
+CORS_ALLOWED_ORIGINS = [
+	"http://0.0.0.0:3000",
+]
 
 CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
+	"DELETE",
+	"GET",
+	"OPTIONS",
+	"PATCH",
+	"POST",
+	"PUT",
 ]
 
 CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
+	'accept',
+	'accept-encoding',
+	'authorization',
+	'content-type',
+	'dnt',
+	'origin',
+	'user-agent',
+	'x-csrftoken',
+	'x-requested-with',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
+	'corsheaders.middleware.CorsMiddleware',  # CORS middleware should be first
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-	'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
