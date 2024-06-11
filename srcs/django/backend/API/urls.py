2e7d3f097b0cd9f12b9e54ffe51cd472e3daf05c
Courinha <antoniocourinha768@gmail.com>
2024-06-11 23:45:21 +0100

    Removed the "Django" container
    there is no point to this container anymore every function was replaced in the "Authentication" container

--- srcs/django/backend/API/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserList, UserDetail, login_view
-
-urlpatterns = [
-	path('User', UserList.as_view()),
-	path('User/<int:pk>', UserDetail.as_view()),
-	path('loginUser', login_view)
-]
