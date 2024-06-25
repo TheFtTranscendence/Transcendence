4e78d1b653e16e801f11259f40bd32e7d9f18572
lopjf <loris.penelon@outlook.com>
2024-06-25 19:57:25 +0200

    added await

--- srcs/solidity/solidity_service/api/views.py
+++ srcs/solidity/solidity_service/api/views.py
@@ -1,12 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
 from .web3_utils import get_number_of_games, add_game
 
-@api_view(['GET'])
-def get_games(request, instance_index):
-    number_of_games = get_number_of_games(instance_index)
-    return Response({'number_of_games': number_of_games})
+# Add an instance (it returns the instance index)
 
+# Add a game
 @api_view(['POST'])
 def add_new_game(request):
     instance_index = request.data.get('instance_index')
@@ -14,5 +13,21 @@ def add_new_game(request):
     player2 = request.data.get('player2')
     score1 = request.data.get('score1')
     score2 = request.data.get('score2')
-    tx_hash = add_game(instance_index, player1, player2, score1, score2)
-    return Response({'tx_hash': tx_hash})
+    tx_hash = await add_game(instance_index, player1, player2, score1, score2)
+    return JsonResponse({'message': 'Game added'}, status=200)
+
+# Add tournament (4 or 8 players)
+
+# Add a tournament game (it also adds it to the array of games with a index of the tournament)
+
+# Get the number of games
+@api_view(['GET'])
+def get_games(request, instance_index):
+    number_of_games = await get_number_of_games(instance_index)
+    return JsonResponse({'number_of_games': number_of_games}, status=200)
+
+# Get all the games (for the history)
+
+# Get the next tournament game to play (returns 2 players)
+
+# Get a tournament ranking
--- srcs/solidity/solidity_service/api/web3_utils.py
+++ srcs/solidity/solidity_service/api/web3_utils.py
@@ -1,4 +1,6 @@
-from web3 import Web3
+import json
+import asyncio
+from web3 import Web3, AsyncHTTPProvider
 from django.conf import settings
 
 # Get from settings
@@ -7,23 +9,23 @@ contract_address = settings.CONTRACT_ADDRESS
 private_key = settings.PRIVATE_KEY
 
 # Initialize Web3
-web3 = Web3(Web3.HTTPProvider(ethereum_node_url))
+web3 = Web3(AsyncHTTPProvider(ethereum_node_url))
 
 # Load the contract ABI
-with open('.../Score.json') as f:
+with open('path/to/Score.json') as f:
     contract_abi = json.load(f)['abi']
 contract = web3.eth.contract(address=contract_address, abi=contract_abi)
 
-def get_number_of_games(instance_index):
-    return contract.functions.getNumberOfGames(instance_index).call()
+async def get_number_of_games(instance_index):
+    return await contract.functions.getNumberOfGames(instance_index).call()
 
-def add_game(instance_index, player1, player2, score1, score2):
+async def add_game(instance_index, player1, player2, score1, score2):
     tx = contract.functions.addGame(instance_index, player1, player2, score1, score2).buildTransaction({
         'chainId': 11155111,
         'gas': 2000000,
         'gasPrice': web3.toWei('50', 'gwei'),
-        'nonce': web3.eth.getTransactionCount(web3.eth.default_account),
+        'nonce': await web3.eth.getTransactionCount(web3.eth.default_account),
     })
     signed_tx = web3.eth.account.sign_transaction(tx, private_key)
-    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    tx_hash = await web3.eth.sendRawTransaction(signed_tx.rawTransaction)
     return tx_hash.hex()
