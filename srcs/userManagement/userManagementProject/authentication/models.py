687f5cdb002f0a4c7b12e1748303464331c56d72
courinha <antoniocourinha768@gmail.com>
2024-07-24 21:32:50 +0100

    fixed merged (I hope)

--- srcs/userManagement/userManagementProject/authentication/models.py
+++ srcs/userManagement/userManagementProject/authentication/models.py
@@ -3,72 +3,55 @@ from django.db import models
 from django.utils import timezone
 
 class customUserManager(UserManager):
-    def _create_user(self, email, username, password, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        username = self.model.normalize_username(username)
-        user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+	def _create_user(self, email, username, password, **extra_fields):
+		if not email:
+			raise ValueError('The Email field must be set')
+		email = self.normalize_email(email)
+		username = self.model.normalize_username(username)
+		user = self.model(email=email, username=username, **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
 
-    def create_user(self, email, username=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, username, password, **extra_fields)
+	def create_user(self, email, username=None, password=None, **extra_fields):
+		extra_fields.setdefault('is_staff', False)
+		extra_fields.setdefault('is_superuser', False)
+		return self._create_user(email, username, password, **extra_fields)
 
-    def create_superuser(self, email, username=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(email, username, password, **extra_fields)
+	def create_superuser(self, email, username=None, password=None, **extra_fields):
+		extra_fields.setdefault('is_staff', True)
+		extra_fields.setdefault('is_superuser', True)
+		if extra_fields.get('is_staff') is not True:
+			raise ValueError('Superuser must have is_staff=True.')
+		if extra_fields.get('is_superuser') is not True:
+			raise ValueError('Superuser must have is_superuser=True.')
+		return self._create_user(email, username, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=False)
-    username = models.CharField(max_length=100, unique=True)
+	email = models.EmailField(unique=False)
+	username = models.CharField(max_length=100, unique=True)
 
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
-    
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    
-    date_joined = models.DateTimeField(default=timezone.now)
-    last_login = models.DateTimeField(blank=True, null=True)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
+	is_superuser = models.BooleanField(default=False)
 
-    groups = models.ManyToManyField(
-        Group,
-        related_name='custom_user_set',
-        blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-        verbose_name='groups'
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='custom_user_set',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        verbose_name='user permissions'
-    )
+	# Smart Contract ID
+	sc_id = models.IntegerField(unique=True, null=True, blank=True)
 
-    objects = customUserManager()
+	avatar = models.ImageField(default='default.jpg', upload_to='')
 
-    USERNAME_FIELD = 'username'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['email']
+	friends = models.ManyToManyField(
+		'self',
+		blank=True,
+		verbose_name='friends'
+	)
 
-    class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+	objects = customUserManager()
 
-    def get_full_name(self):
-        full_name = f'{self.first_name} {self.last_name}'
-        return full_name.strip()
+	USERNAME_FIELD = 'username'
+	EMAIL_FIELD = 'email'
+	REQUIRED_FIELDS = ['email']
 
-    def get_short_name(self):
-        return self.first_name.strip()
+	class Meta:
+		verbose_name = 'User'
+		verbose_name_plural = 'Users'
