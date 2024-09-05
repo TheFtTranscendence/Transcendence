3975edeb9446d0c8fb6203dc4923ab31e04379b1
courinha <antoniocourinha768@gmail.com>
2024-09-05 22:07:31 +0100

    unread_messages

--- srcs/chat/chatApp/views.py
+++ srcs/chat/chatApp/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Chat, Message
 from .serializers import ChatSerializer, MessageSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -25,12 +25,37 @@ class ChatViewSet(viewsets.ModelViewSet):
 		if not chat:
 			chat = Chat.objects.create(user1=user1, user2=user2)
 		serializer = self.get_serializer(chat)
+		data = serializer.data
+
 		if (chat.user1 == user1):
 			chat.user1_unread_messages = 0
 		else:
 			chat.user2_unread_messages = 0
-		chat.save()			
-		return Response(serializer.data)
+		chat.save()	
+
+		return Response(data)
+	
+	def retrieve(self, request, *args, **kwargs):
+		instance = self.get_object()
+		user = request.query_params.get('user', None)
+
+
+		serializer = self.get_serializer(instance)
+		data = serializer.data
+
+		if not user:
+			return Response({"detail": "User query parameter is required."}, status=status.HTTP_400_BAD_REQUEST)
+	
+		if instance.user1 == user:
+			instance.user1_unread_messages = 0
+		elif instance.user2 == user:
+			instance.user2_unread_messages = 0
+		else:
+			return Response({"detail": "User not part of this chat."}, status=status.HTTP_400_BAD_REQUEST)
+		instance.save()
+
+
+		return Response(data)
 			
 
 class MessageViewSet(viewsets.ModelViewSet):
