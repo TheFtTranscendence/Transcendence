3938d15f5f450e82fc03ded6640e59f8b711c6ce
Diogo Dantas goncalves <ddantas-@c2r4s10.42lisboa.com>
2024-10-08 21:49:06 +0100

    added add invite block remove unblock user

--- srcs/frontend/public/chat/chat.js
+++ srcs/frontend/public/chat/chat.js
@@ -8,6 +8,15 @@ function chat_hashchange(event) {
         element.removeEventListener('click', listener)
     })
 
+	window.addButton.removeEventListener('click', chat_addButton)
+	window.removeButton.removeEventListener('click', chat_removeButton)
+	window.blockButton.removeEventListener('click', chat_blockButton)
+	window.unblockButton.removeEventListener('click', chat_unblockButton)
+	window.inviteButton1.removeEventListener('click', chat_inviteButton1)
+	window.inviteButton2.removeEventListener('click', chat_inviteButton2)
+
+	window.confirmButton.removeEventListener('click', chat_confirmButton)
+	
     // Clear the chat list container's contents
     window.chatListContainer.innerHTML = ''
 	window.chatContent.innerHTML = ''
@@ -43,7 +52,7 @@ function sendChatMessage() {
 		chatInput.value = ''
 
 		// Scroll to the bottom of the chat content
-		window.chatContent.scrollTop = window.chatContent.scrollHeight
+		window.chatContent.scrollTop = window.chatCondocument.getElementById('chat-input-buttons').placeholder = "New placeholder text";tent.scrollHeight
 	}
 }
 
@@ -150,12 +159,50 @@ function displayChatList(chatListContainer, friendList) {
 		// Add the click event listener
 		const listener = () => openChat(friend)
 		chatDiv.addEventListener('click', listener)
-
+		
 		// Store the chatDiv and its listener
 		window.chatDivs.push({ element: chatDiv, listener })
-
+		
 		chatListContainer.appendChild(chatDiv)
 	})
+}
+
+function chat_addButton() { document.getElementById('chat-input-buttons').placeholder = "add"; }
+function chat_removeButton() { document.getElementById('chat-input-buttons').placeholder = "remove"; }
+function chat_blockButton() { document.getElementById('chat-input-buttons').placeholder = "block"; }
+function chat_unblockButton() { document.getElementById('chat-input-buttons').placeholder = "unblock"; }
+function chat_inviteButton1() { document.getElementById('chat-input-buttons').placeholder = "pongy"; }
+function chat_inviteButton2() { document.getElementById('chat-input-buttons').placeholder = "fighty"; }
+
+async function chat_confirmButton() {
+
+	const holder = document.getElementById('chat-input-buttons').placeholder
+	const userInput = document.getElementById('chat-input-buttons').value
+	await get_user_info(userInput).then((response) => {
+		console.log('response ', response)
+		user_to_send = response.id
+	}).catch((error) => {
+		alert('User not found', error)
+		return 
+	})
+
+	console.log('holder ', holder)
+
+	switch (holder) {
+		case "add": send_friend_request(user_to_send); alert('Friend request sent')
+			break
+		case "remove": remove_friend(user_to_send); alert('Friend removed')
+			break
+		case "block": block(user_to_send); alert('User blocked')
+			break
+		case "unblock": unblock(user_to_send); alert('User unblocked')
+			break
+		case "pongy": send_game_invite(user_to_send, 'pongy'); alert('Pong invite sent')
+			break
+		case "fighty": send_game_invite(user_to_send, 'fighty'); alert('Fighty invite sent')
+			break
+		default: alert('Select an option first')
+	}
 }
 
 function chat()
@@ -170,6 +217,29 @@ function chat()
 	window.sendButton = document.getElementById('send-button')
 	window.chatInput = document.getElementById('chat-input')
 	window.chatDiv = document.createElement('div')
+
+	window.addButton = document.getElementById('add-button')
+	window.removeButton = document.getElementById('remove-button')
+	window.blockButton = document.getElementById('block-button')
+	window.unblockButton = document.getElementById('unblock-button')
+	window.inviteButton1 = document.getElementById('invite-button-pong')
+	window.inviteButton2 = document.getElementById('invite-button-fighters')
+
+	window.confirmButton = document.getElementById('confirm-button')
+
+
+	
+	window.addButton.addEventListener('click', chat_addButton)
+	window.removeButton.addEventListener('click', chat_removeButton)
+	window.blockButton.addEventListener('click', chat_blockButton)
+	window.unblockButton.addEventListener('click', chat_unblockButton)
+	window.inviteButton1.addEventListener('click', chat_inviteButton1)
+	window.inviteButton2.addEventListener('click', chat_inviteButton2)
+
+	window.confirmButton.addEventListener('click', chat_confirmButton)
+
+	window.chatConfirmInput = document.getElementById('chat-input-buttons')
+
 
 	// Get through the database when implemented
 	window.Messages = []
