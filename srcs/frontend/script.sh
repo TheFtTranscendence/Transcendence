802e1adc98962ebe96f6cdc3c14791d468a43b55
Diogo Dantas goncalves <ddantas-@c2r4s12.42lisboa.com>
2024-10-09 13:44:54 +0100

    nginx tls update

--- srcs/frontend/script.sh
+++ srcs/frontend/script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 
+curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64" && \
+chmod +x mkcert-v*-linux-amd64 && \
+cp mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+
+mkcert $IP
+cp $IP* /etc/nginx/ssl
+
+envsubst < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/nginx.conf
+
+rm /etc/nginx/conf.d/default.conf
 cd /usr/share/nginx/html/
 
 echo "Updating IP"
